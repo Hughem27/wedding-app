1b680d5c6289f3580e6afb018ff89900c13c785c
mark <markhughesster@gmail.com>
2025-07-04 20:48:59 +0100

    i on even know anymore

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+console.log("VITE_API_BASE_URL:", process.env.VITE_API_BASE_URL);
+
 
 // https://vite.dev/config/
 export default defineConfig({
